7853345f278b3f28ae2c2be770b1f694174221ac
Sasa <sasaxxxpajic@gmail.com>
2024-07-20 22:46:29 +0200

    Add Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "@fontsource/vt323"; // Defaults to weight 400
+
+export default function Header() {
+    return(
+        <div className="header">
+            <div>sasathepajic</div>
+            <div>dsgn&dvlpmnt</div>
+        </div>
+    )
+}
