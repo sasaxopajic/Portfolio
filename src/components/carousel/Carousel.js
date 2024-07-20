10f3c940cc9f1fadc5c12b329886ba5b4840f815
Sasa <sasaxxxpajic@gmail.com>
2024-07-20 22:45:54 +0200

    Add Carousel component

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export default function Carousel() {
+    return (
+    <div className="carousel-container">
+        <div>Aesthetics</div>
+        <div>/</div>
+        <div>Functionality</div>
+        <div>/</div>
+        <div>Seamless user-experience</div>
+        <div>/</div>
+    </div>
+
+    )
+}
