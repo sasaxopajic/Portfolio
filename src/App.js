c1a231f3b39e75a04673c3ac8e6431271db32df0
Sasa <sasaxxxpajic@gmail.com>
2024-07-20 22:49:18 +0200

    Add landing page component in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header";
+import './components/header/Header.css';
+import Carousel from './components/carousel/Carousel';
+import './components/carousel/Carousel.css'
+import AboutMe from './components/about-me/AboutMe';
+import './components/about-me/AboutMe.css';
+import Experience from './components/experience/Experience';
+import SelectedWork from './components/selected-work/SelectedWork';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Carousel />
+      <div className="heading">
+        <div>Rocketing</div>
+        <div>your ideas</div>
+        <div>into universe</div>
+      </div>
+      <AboutMe />
+      <Experience/>
+      <SelectedWork/>
+      <Footer/>
     </div>
+
   );
 }
 
