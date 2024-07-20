7da06fe83473f0218961efc3858a223b988acc9b
Sasa <sasaxxxpajic@gmail.com>
2024-07-20 22:46:45 +0200

    Add Footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css';
+import "@fontsource/vt323"; // Defaults to weight 400
+
+export default function Footer(){
+    return (
+        <div className={styles.footer}>
+            all rights reserved @ sasathepajic
+        </div>
+    )
+}
