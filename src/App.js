aca5d2a1283715d72bef7ca421341e335a417184
Sasa <sasaxxxpajic@gmail.com>
2024-07-21 17:01:47 +0200

    Refactor and add routes to App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import './App.css';
-import Header from "./components/header/Header";
 import './components/header/Header.css';
-import Carousel from './components/carousel/Carousel';
 import './components/carousel/Carousel.css'
 import AboutMe from './components/about-me/AboutMe';
 import './components/about-me/AboutMe.css';
+import Home from './components/home/Home'
 import Experience from './components/experience/Experience';
 import SelectedWork from './components/selected-work/SelectedWork';
-import Footer from './components/footer/Footer';
+import Contact from './components/contact/Contact';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Carousel />
-      <div className="heading">
-        <div>Rocketing</div>
-        <div>your ideas</div>
-        <div>into universe</div>
-      </div>
-      <AboutMe />
-      <Experience/>
-      <SelectedWork/>
-      <Footer/>
+
+      <Routes>
+        <Route index element={<Navigate to="/home" replace />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about-me' element={<AboutMe />} />
+        <Route path='/projects' element={<SelectedWork />} />
+        <Route path='/experience' element={<Experience />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
 
   );
