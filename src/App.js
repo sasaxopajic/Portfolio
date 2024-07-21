a9b39fd734ce6438ad645d3f7f32bd7d7f7ca514
Sasa <sasaxxxpajic@gmail.com>
2024-07-21 22:47:39 +0200

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import './components/header/Header.css';
-import './components/carousel/Carousel.css'
 import AboutMe from './components/about-me/AboutMe';
-import './components/about-me/AboutMe.css';
 import Home from './components/home/Home'
 import Experience from './components/experience/Experience';
 import SelectedWork from './components/selected-work/SelectedWork';
 import Contact from './components/contact/Contact';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
 
       <Routes>
         <Route index element={<Navigate to="/home" replace />} />
