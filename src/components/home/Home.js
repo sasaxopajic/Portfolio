af5bb315652bfbbed4fd0cc27c46250dc69d49d3
Sasa <sasaxxxpajic@gmail.com>
2024-07-24 15:19:46 +0200

    Import SCSS module

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,14 @@
 import Header from "../../components/header/Header";
 import Carousel from '../../components/carousel/Carousel';
 import Footer from '../../components/footer/Footer';
+import styles from './Home.module.scss';
 
 export default function Home() {
     return (
         <div>
             <Header />
             <Carousel />
-            <div className="heading">
+            <div className={styles.heading}>
                 <div>Rocketing</div>
                 <div>your ideas</div>
                 <div>into universe</div>
