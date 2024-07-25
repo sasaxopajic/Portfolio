862491c3f425a27295cb5cbbebc3d99a24e5343b
Sasa <sasaxxxpajic@gmail.com>
2024-07-25 16:02:09 +0200

    Fix routing in AppLayout component

--- src/components/app-layout/AppLayout.js
+++ src/components/app-layout/AppLayout.js
@@ -8,35 +8,21 @@ import Navbar from '../navbar/Navbar';
 import Project from '../selected-work/project/Project';
 
 export default function AppLayout() {
-    /* const { isPending, error, data, isFetching } = useQuery({
-      queryKey: ['projects'],
-      queryFn: async () => {
-        const response = await fetch(
-          'http://localhost:3001/projects',
-        )
-        return await response.json()
-      },
-    })
-  
-    if (isPending) return 'Loading...'
-  
-    if (error) return 'An error has occurred: ' + error.message
-   */
     return (
-      <div>
-        <Navbar />
-       
-        <Routes>
-          <Route index element={<Navigate to="/home" replace />} />
-          <Route path='home' element={<Home />} />
-          <Route path='about-me' element={<AboutMe />} />
-          <Route path='projects' element={<SelectedWork />}>
-            <Route path=':id' element={<Project />} />
-          </Route>
-          <Route path='experience' element={<Experience />} />
-          <Route path='contact' element={<Contact />} />
-        </Routes>
-      </div>
+        <div>
+            <Navbar />
+
+            <Routes>
+                <Route index element={<Navigate to="/home" replace />} />
+                <Route path='home' element={<Home />} />
+                <Route path='about-me' element={<AboutMe />} />
+                <Route path='projects' element={<SelectedWork />}>
+                    <Route path="/projects/:projectId" element={<Project />} />
+                </Route>
+                <Route path='experience' element={<Experience />} />
+                <Route path='contact' element={<Contact />} />
+            </Routes>
+        </div>
     )
-  
-  }
+
+}
