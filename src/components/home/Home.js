19bd771dc764af5ae7e2bea294b84ac1b9ba4886
Sasa <sasaxxxpajic@gmail.com>
2024-07-25 16:00:49 +0200

    Animate and decorate Home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,19 +2,23 @@ import Header from "../../components/header/Header";
 import Carousel from '../../components/carousel/Carousel';
 import Footer from '../../components/footer/Footer';
 import styles from './Home.module.scss';
+import { ReactComponent as Icon } from '../../assets/planet.svg';
 
 export default function Home() {
     return (
-        <div>
+        <div className={styles.container}>
             <Header />
             <Carousel />
-            <div className={styles.heading}>
-                <div>Rocketing</div>
-                <div>your ideas</div>
-                <div>into universe</div>
+            <div className={styles.home}>
+                <Icon className={styles.rotate}></Icon>
+                <div className={styles.heading}>
+                    <div>Rocketing</div>
+                    <div>your ideas</div>
+                    <div>into universe</div>
+                </div>
+                <Icon className={styles["rotate-counterclockwise"]}></Icon>
             </div>
             <Footer />
-
         </div>
     )
 }
