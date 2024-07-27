58fd8fadda3cf07e69ca398215e057eb45d687d3
Sasa <sasaxxxpajic@gmail.com>
2024-07-27 14:37:51 +0200

    Fix AppLayout component

--- src/components/app-layout/AppLayout.js
+++ src/components/app-layout/AppLayout.js
@@ -6,23 +6,31 @@ import Contact from '../contact/Contact';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
 import Project from '../selected-work/project/Project';
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+import style from './AppLayout.module.scss';
 
 export default function AppLayout() {
     return (
-        <div>
-            <Navbar />
+        <div className={style.container}>
+            <div className={style.content}>
+                <Header />
+                <Navbar />
+                <Routes>
+                    <Route index element={<Navigate to="/home" replace />} />
+                    <Route path='home' element={<Home />} />
+                    <Route path='about-me' element={<AboutMe />} />
+                    <Route path='projects' element={<SelectedWork />}>
+                        <Route path="/projects/:projectId" element={<Project />} />
+                    </Route>
+                    <Route path='experience' element={<Experience />} />
+                    <Route path='contact' element={<Contact />} />
+                </Routes>
+            </div>
+            <Footer />
 
-            <Routes>
-                <Route index element={<Navigate to="/home" replace />} />
-                <Route path='home' element={<Home />} />
-                <Route path='about-me' element={<AboutMe />} />
-                <Route path='projects' element={<SelectedWork />}>
-                    <Route path="/projects/:projectId" element={<Project />} />
-                </Route>
-                <Route path='experience' element={<Experience />} />
-                <Route path='contact' element={<Contact />} />
-            </Routes>
         </div>
+
     )
 
 }
