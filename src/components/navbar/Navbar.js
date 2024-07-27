6b6d6d4519226347d281200909ed03b45c8330d9
Sasa <sasaxxxpajic@gmail.com>
2024-07-27 14:39:38 +0200

    Edit Navbar CSS

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
-    return (
-        <nav>
-            <ul className={styles.container}>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/experience">Experience</Link></li>
-                <li><Link to="/about-me">About me</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
+    const location = useLocation();
+    const currentPath = location.pathname;
+    const isProjectsActive = currentPath.startsWith('/projects');
 
-    )
+    return (
+        <div className={styles.container}>
+            <nav >
+                <ul>
+                    <li className={currentPath === "/home" ? styles.active : ''}>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li className={isProjectsActive ? styles.active : ''}>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li className={currentPath === "/experience" ? styles.active : ''}>
+                        <Link to="/experience">Experience</Link>
+                    </li>
+                    <li className={currentPath === "/about-me" ? styles.active : ''}>
+                        <Link to="/about-me">About me</Link>
+                    </li>
+                    <li className={currentPath === "/contact" ? styles.active : ''}>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
