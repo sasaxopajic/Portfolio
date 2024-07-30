6aea3b6912b8c315d3dc4e8784ce2518e8b27dab
Sasa <sasaxxxpajic@gmail.com>
2024-07-30 17:09:09 +0200

    Destructure and develop SelectedWork component to show all projects on screen

--- src/components/selected-work/SelectedWork.js
+++ src/components/selected-work/SelectedWork.js
@@ -1,15 +1,41 @@
-import styles from './SelectedWork.module.scss'
+import React, { useEffect } from 'react';
+import { useProjectsQuery } from '../../services/queries/projects.query';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
+import styles from './SelectedWork.module.scss';
+import ScrollProgressBar from '../scroll-progress-bar/ScrollProgressBar';
+import ProjectNavbar from '../selected-work/project-navbar/ProjectNavbar'
 
-export default function SelectedWork(){
-    return(
-        <div>
-            <div className={styles.heading}>Selected work</div>
-            <ul className={styles.grid}>
-                <li>Gitworks</li>
-                <li>barbershop</li>
-                <li>movies</li>
-                <li>zakazivko</li>
-            </ul>
+const SelectedWork = () => {
+    const { data, isLoading, isError, error } = useProjectsQuery();
+    const location = useLocation();
+    const { projectId } = useParams();
+    const isProjectPage = location.pathname.startsWith('/projects/');
+
+    useEffect(() => {
+        if (isProjectPage) {
+            window.scrollTo(0, 0); // Scroll to the top of the page
+        }
+    }, [projectId, isProjectPage]);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <div className={styles.container}>
+            <ProjectNavbar
+                projects={data}
+                activeProjectId={projectId}
+                isProjectPage={isProjectPage}
+            />
+            {isProjectPage && <ScrollProgressBar />}
+            <Outlet />
         </div>
-    )
-}
+    );
+};
+
+export default SelectedWork;
