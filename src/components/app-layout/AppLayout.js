cdada6a199e449bb9a537dcddd48eff7846be612
Sasa <sasaxxxpajic@gmail.com>
2024-07-30 17:03:56 +0200

    Destruc AppLayout component

--- src/components/app-layout/AppLayout.js
+++ src/components/app-layout/AppLayout.js
@@ -1,14 +1,8 @@
-import AboutMe from '../about-me/AboutMe';
-import Home from '../home/Home';
-import Experience from '../experience/Experience';
-import SelectedWork from '../selected-work/SelectedWork';
-import Contact from '../contact/Contact';
-import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
-import Project from '../selected-work/project/Project';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import style from './AppLayout.module.scss';
+import AppRoutes from '../routes/AppRoutes';
 
 export default function AppLayout() {
     return (
@@ -16,16 +10,7 @@ export default function AppLayout() {
             <div className={style.content}>
                 <Header />
                 <Navbar />
-                <Routes>
-                    <Route index element={<Navigate to="/home" replace />} />
-                    <Route path='home' element={<Home />} />
-                    <Route path='about-me' element={<AboutMe />} />
-                    <Route path='projects' element={<SelectedWork />}>
-                        <Route path="/projects/:projectId" element={<Project />} />
-                    </Route>
-                    <Route path='experience' element={<Experience />} />
-                    <Route path='contact' element={<Contact />} />
-                </Routes>
+                <AppRoutes/>
             </div>
             <Footer />
 
